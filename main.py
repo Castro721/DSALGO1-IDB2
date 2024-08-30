1b05fd79833ce20adc589c87b931ab7f11b2c606
castro_mh <castro_mh@f213.ub.lan>
2024-08-30 15:56:09 +0800

    Including my submission for Exersice 1

---
+++ main.py
@@ -0,0 +1,22 @@
+#100 pesos wii
+'''amtmoney=float(input("How much money do you have ? "))
+numWii=int(amtmoney/100)
+needWii=100-(amtmoney%100)
+print("You can only afford", numWii, "Nintendo Wiis""\nYou need",needWii ,"more pesos to afford another Nintendo Wiis")'''
+
+'''factorial =int(input("Please enter a number: "))
+factorialAns=1
+for i in range(1, factorial+1):
+    factorialAns=factorialAns*i
+print("The factorial of", factorial, "is", factorialAns)'''
+
+counter=0
+factor=int(input("Enter a number dat you want to get the factors: "))
+factorList=[]
+for x in range(1,factor+1):
+    if factor%x==0:
+        #print(x, "is a factor of", factor,".")
+        factorList.append(x)
+        counter=counter+1
+print("These are the factors of", factor, "\n",factorList)
+print("Number", factor, "have", counter, "factors")
